185518e7628c6e36fcfbd32ef72d88a2d8bd0e23
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-24 20:46:53 +0400

    Temporary solution passes for 8 tests and fails for 3

--- src/main/kotlin/Transpose.kt
+++ src/main/kotlin/Transpose.kt
@@ -1,6 +1,19 @@
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
-        TODO("Implement this to complete the task")
+        val result = mutableListOf<String>()
+
+        for (word in input) {
+            word.forEachIndexed { index, char ->
+                val current = result.elementAtOrNull(index)
+                if (current != null) {
+                    result[index] = current + char.toString()
+                } else {
+                    result.add(char.toString())
+                }
+            }
+        }
+
+        return result
     }
 }
