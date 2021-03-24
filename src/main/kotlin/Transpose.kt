ec4a468cd77912c663efc68bdd48c4197ccb81d9
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-24 21:27:12 +0400

    Only 1 unit test still fails

--- src/main/kotlin/Transpose.kt
+++ src/main/kotlin/Transpose.kt
@@ -1,15 +1,22 @@
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
+        // We need to find the length of the longest word in the input
+        val longestWord = input.maxBy { arr ->
+            arr.length
+        }?.length
+
         val result = mutableListOf<String>()
 
-        for (word in input) {
-            word.forEachIndexed { index, char ->
-                val current = result.elementAtOrNull(index)
-                if (current != null) {
-                    result[index] = current + char.toString()
+        for ((wordIndex, word) in input.withIndex()) {
+            word.forEachIndexed { charIndex, char ->
+                val currentChar = result.elementAtOrNull(charIndex)
+                if (currentChar != null) {
+                    result[charIndex] = currentChar + char.toString()
                 } else {
-                    result.add(char.toString())
+                    // The first word has been transposed, and we are now dealing with following word
+                    // The assumption here is that
+                    result.add(" ".repeat(wordIndex) + char.toString())
                 }
             }
         }
