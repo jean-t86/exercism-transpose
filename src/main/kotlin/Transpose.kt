3bdd74226d892dfa0818a472159281804769994e
J.Tadebois <jpm.tadebois@gmail.com>
2021-03-24 21:37:59 +0400

    Add completed solution

--- src/main/kotlin/Transpose.kt
+++ src/main/kotlin/Transpose.kt
@@ -1,24 +1,27 @@
 object Transpose {
 
     fun transpose(input: List<String>): List<String> {
-        // We need to find the length of the longest word in the input
-        val longestWord = input.maxBy { arr ->
-            arr.length
-        }?.length
-
         val result = mutableListOf<String>()
 
         for ((wordIndex, word) in input.withIndex()) {
+
             word.forEachIndexed { charIndex, char ->
+
                 val currentChar = result.elementAtOrNull(charIndex)
                 if (currentChar != null) {
-                    result[charIndex] = currentChar + char.toString()
+                    val currentCharLength = currentChar.length
+                    if (currentCharLength != wordIndex) {
+                        // Add white space padding
+                        result[charIndex] = "$currentChar $char"
+                    } else {
+                        result[charIndex] = currentChar + char.toString()
+                    }
                 } else {
-                    // The first word has been transposed, and we are now dealing with following word
-                    // The assumption here is that
                     result.add(" ".repeat(wordIndex) + char.toString())
                 }
+
             }
+
         }
 
         return result
